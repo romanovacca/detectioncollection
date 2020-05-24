98f43ceb4ddd51af39fd6566685a8dc57a1380b4
romanovacca <romanovacca@gmail.com>
2020-05-24 18:06:30 +0200

    added loading weights

--- src/core/models/internal/yolo/v3/test/test_darknet.py
+++ src/core/models/internal/yolo/v3/test/test_darknet.py
@@ -1,5 +1,7 @@
 import unittest
 from src.core.models.internal.yolo.v3.darknet import *
+from src.core.models.internal.yolo.v3.util import Handler
+
 
 class Darknettests(unittest.TestCase):
 
@@ -7,18 +9,17 @@ class Darknettests(unittest.TestCase):
         self.image_path = "data/test/images/"
         self.config_file_location = "../cfg/yolov3.cfg"
 
-
     def tearDown(self):
         pass
 
-    # def test_parse_cfg(self):
-    #     """
-    #     Test that when the model is initialized without model name, the default model gets selected.
-    #     """
-    #     blocks = parse_cfg(self.config_file_location)
-    #     self.assertIsInstance(blocks, list)
-    #     self.assertIsInstance(blocks[0], dict)
-    #     self.assertEqual(len(blocks), 108)
+    def test_parse_cfg(self):
+        """
+        Test that when the model is initialized without model name, the default model gets selected.
+        """
+        blocks = parse_cfg(self.config_file_location)
+        self.assertIsInstance(blocks, list)
+        self.assertIsInstance(blocks[0], dict)
+        self.assertEqual(len(blocks), 108)
 
     def test_create_pytorch_modules(self):
         """
@@ -29,3 +30,17 @@ class Darknettests(unittest.TestCase):
         self.assertIsInstance(modules[0], dict)
         self.assertEqual(str(type(modules[1])), "<class 'torch.nn.modules.container.ModuleList'>")
         self.assertEqual(modules[0]["type"], "net")
+
+    def test_model_initialization(self):
+        model = Darknet(self.config_file_location)
+        self.assertEqual(str(type(model)), "<class 'src.core.models.internal.yolo.v3.darknet.Darknet'>")
+
+    def test_model_initialization(self):
+        handler = Handler()
+        inp = handler.get_test_input()
+
+        model = Darknet(self.config_file_location)
+        pred = model(inp, torch.cuda.is_available())
+        self.assertEqual(pred.shape[0], 1)
+        self.assertEqual(pred.shape[1], 22743)
+        self.assertEqual(pred.shape[2], 85)
